04d9ed65cef1707b1e6f090160fce1cfeff9feb2
sudoAptIPedro <94088647+sudoAptIPedro@users.noreply.github.com>
2021-11-10 23:26:14 -0300

    Add files via upload

---
+++ consoleTeste.php
@@ -0,0 +1,46 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="/htdocys/phpProjectsAndStudies/php/phpProcedural/css/styleY.css">
+    <title>127.0.0.1</title>
+</head>
+<body class="background">
+    <div class="container">
+        <main>
+            <section class="titulo">
+                <header><h1>WELCOME TO PHP CONSOLE PROJECT </h1></header>
+            </section>
+            <section> 
+                <h2 class="title2">CHOSE YOUR DESTINY</h2>           
+                <nav class="navigationMenu">
+                    <ul>
+                        <li><a href="/htdocys/phpProjectsAndStudies/" target="_blank">HOME</a></li>
+                        <li><a href="#console" target="_blank">CONSOLE</a></li>
+                        <li><a href="/htdocys/phpProjectsAndStudies/php/phpProcedural/forms.php" target="_blank">Entre em contato!!</a></li>
+                        <li><a href="https://www.google.com.br/" target="_blank">GOOGLE</a></li>
+                        <li style="float:right"><a class="active" href="https://www.linkedin.com/in/pedro-arthur-5518721a5/" target="_blank">About</a></li>
+                    </ul>
+                </nav>
+            </section>   
+            <section class="colunas">
+                <div class="captain">
+                   <div class="coluna Center" id="#center">
+                        <h2>PS C:\Users\arthu></h2>
+                        <p> > </p>
+                        <p>
+                            <strong>
+                                <?php
+                                    echo "Wake up, Neo".'<br>';
+                                    echo "<b>Matrix has you...</b>".'<br>';
+                                ?>
+                            </strong>
+                        </p>
+                    </div>
+            </section> 
+        </main>
+    </div>
+</body>
+</html>
